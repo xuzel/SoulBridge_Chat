54f11de88fd86f4f8f4fd6d1310c846010475436
xuzel <sudooodus@gmail.com>
2024-04-29 04:30:47 +0800

    make come command for v2

--- v2/main.py
+++ v2/main.py
@@ -6,3 +6,11 @@ from langchain_community.llms import Ollama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from streamlit_login_auth_ui.widgets import __login__
+
+
+# agent class
+
+
+# log in
+
+# main page
