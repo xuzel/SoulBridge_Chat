8572d0d619656e3ca6fd4f5698821bfaf62436b9
EllaYng <geyanghou@gmail.com>
2024-04-29 02:42:52 +0800

    fix app error

--- SoulBridge_Chat/settings.py
+++ SoulBridge_Chat/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    "llm_chat_v1.apps.LlmChatV1Config",
 ]
 
 MIDDLEWARE = [
